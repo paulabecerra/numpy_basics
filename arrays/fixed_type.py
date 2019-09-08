2770faa7e9e833220bdfa33942bd28816c5e2e63
paulabecerra <paulabecerrasalcedo@gmail.com>
2019-09-08 17:01:38 -0500

    I added new examples on how types of data change in Numpy arrays and how to create multidimensional arrays

--- arrays/fixed_type.py
+++ arrays/fixed_type.py
@@ -1,6 +1,22 @@
 #Fixed-type arrays with Python
 import numpy as np
 import array
-L = list(range(10))
-A = array.array('i', L)
-print(A)
+A = list(range(10))
+B = array.array('i', A)
+print(B)
+
+#Creating Arrays from Python Lists
+#Integer arrays:
+C = [1,2,3,4,5,6]
+D = np.array(C)
+print(D)
+
+#If the data used do not have the same type Numpy will upcast if possible:
+E = np.array([3.2, 4, 5, 6, 7])
+print(E)
+#All values in this example are converted to floating values
+
+#Unlike Python, Numpy arrays can be explicitly multidimensional:
+#Nested lists result in multidimensional arrays:
+F = np.array([range(i, i+3) for i in [2, 4, 6]])
+print(F)
